a5a3490a3bc9ecb66a2fd6e6846fba3bb74c71d6
Orve-oss <125040265+Orve-oss@users.noreply.github.com>
2025-02-15 23:15:24 +0000

    Template integration

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'events';
+    protected $fillable = [
+        'name', 'location', 'description', 'start_date', 'end_date'
+    ];
 }
