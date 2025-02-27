51649ad73c5235af94d30ba67a33d3a44d725521
Orve-oss <125040265+Orve-oss@users.noreply.github.com>
2025-02-27 00:58:23 +0000

    Parkings setup (first part)

--- app/Http/Controllers/ParkingController.php
+++ app/Http/Controllers/ParkingController.php
@@ -2,16 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Parking;
 use Illuminate\Http\Request;
 
 class ParkingController extends Controller
 {
     //
-    public function index(){
-        return view('parkings.index');
+    public function index()
+    {
+        $parkings = Parking::all();
+        return view('parkings.index', compact('parkings'));
     }
 
     public function create(){
         return view('parkings.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'event_id' => 'required|exists:events,id',
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'total_place' => 'required|integer|min:1',
+        ]);
+
+        $parking = new Parking();
+        $parking->event_id = $request->event_id;
+        $parking->name = $request->name;
+        $parking->description = $request->description;
+        $parking->total_place = $request->total_place;
+        $parking->save();
+
+        return redirect()->route('parkings.index')->with('success', 'Parking created successfully.');
+    }
+
+    public function show($id)
+    {
+        $parking = Parking::findOrFail($id);
+        return view('parkings.show', compact('parking'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'event_id' => 'required|exists:events,id',
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'total_place' => 'required|integer|min:1',
+        ]);
+
+        $parking = Parking::findOrFail($id);
+        $parking->event_id = $request->event_id;
+        $parking->name = $request->name;
+        $parking->description = $request->description;
+        $parking->total_place = $request->total_place;
+        $parking->save();
+
+        return redirect()->route('parkings.index')->with('success', 'Parking updated successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $parking = Parking::findOrFail($id);
+        $parking->delete();
+
+        return redirect()->route('parkings.index')->with('success', 'Parking deleted successfully.');
     }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -111,6 +111,27 @@
               </ul>
             </div>
           </li>
+          <li class="nav-item">
+            <a data-bs-toggle="collapse" href="#sidebarLayouts">
+              <i class="fas fa-user"></i>
+              <p>Reservations</p>
+              <span class="caret"></span>
+            </a>
+            <div class="collapse" id="sidebarLayouts">
+              <ul class="nav nav-collapse">
+                <li>
+                  <a href="/user">
+                    <span class="sub-item">Liste des reservations</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="icon-menu.html">
+                    <span class="sub-item">Icon Menu</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
